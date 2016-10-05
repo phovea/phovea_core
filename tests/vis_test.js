66ae9afe1062816eecdf99ce13db9cc63bb9e826
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-10-05 17:39:20 -0400

    Stub tests of vis

---
+++ tests/vis_test.js
@@ -0,0 +1,76 @@
+define(["require", "exports", 'vis'], function (require, exports, vis) {
+  exports.test = function(){
+
+    QUnit.module('vis.AVisInstance', function() {
+
+      QUnit.test('fields', function(assert) {
+        assert.deepEqual(Object.keys(new vis.AVisInstance()), [
+          "handlers",
+          "id",
+          "_built"
+        ], true);
+      });
+
+      // TODO: Test methods
+
+    });
+
+
+    QUnit.module('vis.assignVis', function() {
+
+      QUnit.test('no arg', function(assert) {
+        assert.throws( // TODO
+          function() {
+            vis.assignVis();
+          },
+          TypeError
+        );
+      });
+
+      QUnit.test('one arg', function(assert) {
+        assert.throws( // TODO
+          function() {
+            vis.assignVis('node');
+          },
+          TypeError
+        );
+      });
+
+      QUnit.test('two arg', function(assert) {
+        assert.throws( // TODO
+          function() {
+            vis.assignVis('node', 'vis');
+          },
+          TypeError
+        );
+      });
+
+    });
+
+
+
+    QUnit.module('vis.list', function() {
+
+      QUnit.test('no arg', function(assert) {
+        assert.throws( // TODO
+          function() {
+            vis.list();
+          },
+          TypeError
+        );
+      });
+
+      QUnit.test('one arg', function(assert) {
+        assert.throws( // TODO
+          function() {
+            vis.list('TODO: datatypes.IDataType');
+          },
+          TypeError
+        );
+      });
+
+    });
+
+  }
+});
+
