ccb93cf593475168e5d513ea1bb501fe3a3c99c9
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-10-13 15:12:46 -0400

    test fix? + session tests

--- tests/tests/session_test.js
+++ tests/tests/session_test.js
@@ -15,11 +15,30 @@ define(["require", "exports", 'session'], function (require, exports, session) {
       QUnit.test('methods', function(assert) {
         var key = 'test-key';
         assert.equal(session.has(key), false);
+
         session.store(key, 'foobar');
         assert.equal(session.has(key), true);
         assert.equal(session.retrieve(key), 'foobar');
+
+        session.store(key, 'barfoo');
+        assert.equal(session.has(key), true);
+        assert.equal(session.retrieve(key), 'barfoo');
+
         session.remove(key);
         assert.equal(session.has(key), false);
+      });
+
+      QUnit.module('datatypes', function(assert) {
+        function assert_store(data) {
+          QUnit.test(typeof data, function(assert) {
+            var key = 'key';
+            session.store(key, data);
+            assert.deepEqual(session.retrieve(key), data);
+          });
+        }
+        assert_store(1);
+        assert_store(['array']);
+        assert_store({'hash': true});
       });
 
     });
