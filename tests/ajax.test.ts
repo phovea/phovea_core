617e30dcbae98f3e6f318d636ef2d6cb34e707d4
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-11-07 17:06:19 -0500

    More tests

--- tests/ajax.test.ts
+++ tests/ajax.test.ts
@@ -1,5 +1,16 @@
 import {api2absURL} from '../src/ajax';
 
 describe('api2absURL', () => {
-  it('no query', () => expect(api2absURL('/path')).toEqual('/api/path'));
+  describe('one arg', () => {
+    it('no query', () => expect(api2absURL('/path')).toEqual('/api/path'));
+  });
+  describe('empty query', () => {
+    it('no query', () => expect(api2absURL('/path', {})).toEqual('/api/path'));
+  });
+  describe('query', () => {
+    it('no query', () => expect(api2absURL('/path', {foo: 'bar'})).toEqual('/api/path?foo=bar'));
+  });
+  describe('url w/ query', () => {
+    it('no query', () => expect(api2absURL('/path?query=fake', {foo: 'bar'})).toEqual('/api/path?query=fake&foo=bar'));
+  });
 });
