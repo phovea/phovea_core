b4d0f2e50eb7d6590a1e1d51fe484636ca920bcf
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-14 10:39:30 +0100

    yo phovea:update + changes for webpack2.beta26

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ const banner = '/*! ' + (pkg.title || pkg.name) + ' - v' + pkg.version + ' - ' +
 
 //list of loaders and their mappings
 const webpackloaders = [
-  {test: /\.scss$/, loader: 'style!css!sass'},
+  {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'},
   {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
   {test: /\.json$/, loader: 'json-loader'},
   {
@@ -204,7 +204,7 @@ function generateWebpack(options) {
     base.plugins.push(p);
     base.module.loaders[0] = {
       test: /\.scss$/,
-      loader: p.extract(['css', 'sass'])
+      loader: p.extract(['css-loader', 'sass-loader'])
     };
   }
   if (options.commons) {
