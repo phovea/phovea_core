9149f6c73092580d4fc9d9144787d5035a9a73d3
mccalluc <mccalluc@users.noreply.github.com>
2016-11-23 09:07:57 -0500

    geom tests (#18)
    * Sam fixed the stack-overflow problem
    
    * Circle corners: This behavior does not seem correct
    
    * bs and intersects; Executed 61 of 61 SUCCESS
    
    * radius, x, y. Executed 65 of 65 SUCCESS
    
    * Fix corner tests. Executed 70 of 70 SUCCESS
    
    * Collapse describe to it
    
    * I will do main in a separate branch
    
    * rect tests
    
    * ellipse tests
    
    * Add CR at end to make lint happy
    
    * remove the old file / todos in the new file
    
    * polygon
    
    * Finish line
    
    * semicolon

--- tests/geom.test.ts
+++ tests/geom.test.ts
@@ -1,4 +1,5 @@
-import {Circle} from '../src/geom';
+import {Circle, Rect, Ellipse, Polygon, Line, CORNER} from '../src/geom';
+import {Vector2D} from '../src/2D';
 
 describe('Circle', () => {
   var circle = new Circle(1,1,1);
@@ -13,87 +14,290 @@ describe('Circle', () => {
     // TODO: more
   });
 
-  // TODO: "Circle asIntersectionParams encountered a declaration exception FAILED"
-  // describe('asIntersectionParams', () => {
-  //   var params = circle.asIntersectionParams();
-  //   it('name', () => expect(params.name).toEqual('Circle'));
-  //   it('name', () => expect(params.params[0].x).toEqual(1));
-  //   it('name', () => expect(params.params[0].y).toEqual(1));
-  //   it('name', () => expect(params.params[1]).toEqual(1));  // TODO: Is this the radius?
-  //   // TODO: more
-  // });
+  describe('asIntersectionParams', () => {
+    var params = circle.asIntersectionParams();
+    it('name', () => expect(params.name).toEqual('Circle'));
+    it('x', () => expect(params.params[0].x).toEqual(1));
+    it('y', () => expect(params.params[0].y).toEqual(1));
+    it('r', () => expect(params.params[1]).toEqual(1));
+  });
 
-  // TODO: "RangeError: Maximum call stack size exceeded"
-  // describe('center', () => {
-  //   it('x', () => expect(circle.center.x).toEqual(1));
-  //   it('y', () => expect(circle.center.y).toEqual(1));
-  // });
+  describe('corner', () => {
+    function corner(label, expected) {
+      it(label, () => expect(circle.corner(CORNER[label]).toString()).toEqual(expected));
+    }
+    corner('NW', '0,0');
+    corner('NE', '2,0');
+    corner('SE', '2,2');
+    corner('SW', '0,2');
+    corner('N', '1,0');
+    corner('S', '1,2');
+    corner('E', '2,1');
+    corner('W', '0,1');
+  });
 
-  // QUnit.test('bs', function(assert) { TODO: What does this mean?
-  //   assert.deepEqual(circle.bs(), '???');
-  // });
+  describe('intersects', () => {
+    it('self', () => expect(circle.intersects(new Circle(1,1,1))));
+    it('outside', () => expect(! circle.intersects(new Circle(-1,-1,1))));
+    it('touch', () => expect(circle.intersects(new Circle(-1,1,1))));
+    it('overlap', () => expect(circle.intersects(new Circle(0,0,1))));
+    it('smaller', () => expect(! circle.intersects(new Circle(1,1,0.5))));
+    it('larger', () => expect(! circle.intersects(new Circle(1,1,2))));
+  });
 
-  // TODO: "RangeError: Maximum call stack size exceeded"
-  // describe('corner', () => {
-  //   var corner = circle.corner(CORNER.NW);
-  //   it('toString', () => expect(corner.toString).toEqual('1,1'));
-  // });
+  it('radius', () => expect(circle.radius).toEqual(1));
+  it('toString', () => expect(circle.toString()).toEqual('Circle(x=1,y=1,radius=1)'));
+  it('x', () => expect(circle.x).toEqual(1));
+  it('y', () => expect(circle.y).toEqual(1));
+  it('xy', () => expect(circle.xy.toString()).toEqual('1,1'));
+  // "bounding sphere"
+  it('bs', () => expect(circle.bs().toString()).toEqual('Circle(x=1,y=1,radius=1)'));
+  it('center', () => expect(circle.center.toString()).toEqual('1,1'));
 
-  // TODO: "RangeError: Maximum call stack size exceeded"
-  // describe('intersects', () => {
-  //   it('self', () => expect(circle.intersects(new Circle(1,1,1))))
-  // });
+  // TODO: Perhaps a transformed circle should be an ellipse?
+  it('transform', () => expect(circle.transform([2,0],0).toString()).toEqual('Circle(x=2,y=0,radius=1)'));
 
-      //   QUnit.test('intersects', function(assert) {
-      //     function assert_intersect(x,y,r,reverse) {
-      //       if (typeof reverse == 'undefined') {
-      //         reverse = true;
-      //       }
-      //       var other = new geom.Circle(x, y, r);
-      //       var intersection = circle.intersects(other);
-      //       if (!reverse) {
-      //         assert.ok(!intersection.intersects);
-      //       } else {
-      //         assert.ok(intersection.intersects);
-      //       }
-      //     }
-      //     assert_intersect(1,1,1); // same
-      //     assert_intersect(-1,-1,1, false); // outside
-      //     assert_intersect(-1,1,1); // touch
-      //     assert_intersect(0,0,1); // overlap
-      //     // TODO: Bug? Maybe intersection means the line of the circle edge,
-      //     // rather than the interior of the disk?
-      //     assert_intersect(1,1,0.5, false); // smaller
-      //     assert_intersect(1,1,2, false); // larger
-      //     // TODO: Test other methods of intersection object
-      //   });
-      //   QUnit.test('radius', function(assert) {
-      //     assert.deepEqual(circle.radius, 1);
-      //   });
-      //   // QUnit.test('shift', function(assert) {
-      //   //   assert.deepEqual(circle.shift(), '???');
-      //   // });
-      //   // QUnit.test('shiftImpl', function(assert) {
-      //   //   assert.deepEqual(circle.shiftImpl(), '???');
-      //   // });
-      //   QUnit.test('toString', function(assert) {
-      //     assert.deepEqual(circle.toString(), 'Circle(x=1,y=1,radius=1)');
-      //   });
-      //   // QUnit.test('transform', function(assert) {
-      //   //   assert.deepEqual(circle.transform(), '???');
-      //   // });
-      //   QUnit.test('x', function(assert) {
-      //     assert.deepEqual(circle.x, 1);
-      //   });
-      //   QUnit.test('y', function(assert) {
-      //     assert.deepEqual(circle.y, 1);
-      //   });
-      //   QUnit.test('xy', function(assert) {
-      //     // TODO: How is this different from .center?
-      //     assert.deepEqual(circle.xy.x, 1);
-      //     assert.deepEqual(circle.xy.y, 1);
-      //     // TODO: more
-      //   });
-      // });
+  // it('shift', () => expect(circle.shift()).toEqual('???'));
+  // it('shiftImpl', () => expect(circle.shiftImpl()).toEqual('???'));
+  // TODO: shiftImpl modifies object in place. Would immutability be a good thing?
+});
+
+describe('Rect', () => {
+  var rect = new Rect(0,0,2,2);
+  describe('aabb', () => {
+    var aabb = rect.aabb();
+    it('x', () => expect(aabb.x).toEqual(0));
+    it('x2', () => expect(aabb.x2).toEqual(2));
+    it('y', () => expect(aabb.y).toEqual(0));
+    it('y2', () => expect(aabb.y2).toEqual(2));
+    it('h', () => expect(aabb.h).toEqual(2));
+    it('w', () => expect(aabb.w).toEqual(2));
+    // TODO: more
+  });
+
+  describe('asIntersectionParams', () => {
+    var params = rect.asIntersectionParams();
+    it('name', () => expect(params.name).toEqual('Rectangle'));
+    it('x', () => expect(params.params[0].x).toEqual(0));
+    it('y', () => expect(params.params[0].y).toEqual(0));
+    it('x', () => expect(params.params[1].x).toEqual(2));
+    it('y', () => expect(params.params[1].y).toEqual(2));
+  });
+
+  describe('corner', () => {
+    function corner(label, expected) {
+      it(label, () => expect(rect.corner(CORNER[label]).toString()).toEqual(expected));
+    }
+    corner('NW', '0,0');
+    corner('NE', '2,0');
+    corner('SE', '2,2');
+    corner('SW', '0,2');
+    corner('N', '1,0');
+    corner('S', '1,2');
+    corner('E', '2,1');
+    corner('W', '0,1');
+  });
+
+  describe('intersects', () => {
+    it('self', () => expect(rect.intersects(new Rect(0,0,2,2))));
+    it('outside', () => expect(! rect.intersects(new Rect(-1,-1,4,4))));
+    it('inside', () => expect(! rect.intersects(new Rect(0.5,0.5,1,1))));
+    it('touch', () => expect(rect.intersects(new Rect(-1,-1,1,1))));
+    it('overlap', () => expect(rect.intersects(new Rect(-1,-1,2,2))));
+  });
+
+  it('toString', () => expect(rect.toString()).toEqual('Rect(x=0,y=0,w=2,h=2)'));
+  it('x', () => expect(rect.x).toEqual(0));
+  it('y', () => expect(rect.y).toEqual(0));
+  it('xy', () => expect(rect.xy.toString()).toEqual('0,0'));
+  // "bounding sphere"
+  it('bs', () => expect(rect.bs().toString()).toMatch(/Circle.x=1,y=1,radius=2.828/)); // TODO: radius twice what it should be?
+  it('center', () => expect(rect.center.toString()).toEqual('1,1'));
+
+  it('transform', () => expect(rect.transform([2,1],0).toString()).toEqual('Rect(x=0,y=0,w=4,h=2)'));
+
+  // it('shift', () => expect(rect.shift()).toEqual('???'));
+  // it('shiftImpl', () => expect(rect.shiftImpl()).toEqual('???'));
+  // TODO: shiftImpl modifies object in place. Would immutability be a good thing?
+});
+
+describe('Ellipse', () => {
+  var ellipse = new Ellipse(1,2,1,2);
+  describe('aabb', () => {
+    var aabb = ellipse.aabb();
+    it('x', () => expect(aabb.x).toEqual(0));
+    it('x2', () => expect(aabb.x2).toEqual(2));
+    it('y', () => expect(aabb.y).toEqual(0));
+    it('y2', () => expect(aabb.y2).toEqual(4));
+    it('h', () => expect(aabb.h).toEqual(4));
+    it('w', () => expect(aabb.w).toEqual(2));
+    // TODO: more
+  });
+
+  describe('asIntersectionParams', () => {
+    var params = ellipse.asIntersectionParams();
+    it('name', () => expect(params.name).toEqual('Ellipse'));
+    it('x', () => expect(params.params[0].x).toEqual(1));
+    it('y', () => expect(params.params[0].y).toEqual(2));
+    it('w', () => expect(params.params[1]).toEqual(1));
+    it('h', () => expect(params.params[2]).toEqual(2));
+  });
+
+  describe('corner', () => {
+    function corner(label, expected) {
+      it(label, () => expect(ellipse.corner(CORNER[label]).toString()).toEqual(expected));
+    }
+    corner('NW', '0,0');
+    corner('NE', '2,0');
+    corner('SE', '2,4');
+    corner('SW', '0,4');
+    corner('N', '1,0');
+    corner('S', '1,4');
+    corner('E', '2,2');
+    corner('W', '0,2');
+  });
+
+  describe('intersects', () => {
+    it('self', () => expect(ellipse.intersects(new Rect(0,0,2,2))));
+    it('outside', () => expect(! ellipse.intersects(new Rect(-1,-1,4,4))));
+    it('inside', () => expect(! ellipse.intersects(new Rect(0.5,0.5,1,1))));
+    it('touch', () => expect(ellipse.intersects(new Rect(-1,-1,1,1))));
+    it('overlap', () => expect(ellipse.intersects(new Rect(-1,-1,2,2))));
+  });
+
+  it('toString', () => expect(ellipse.toString()).toEqual('Ellipse(x=1,y=2,radiusX=1NaN2)')); // TODO: NaN is bad
+  it('x', () => expect(ellipse.x).toEqual(1));
+  it('y', () => expect(ellipse.y).toEqual(2));
+  it('xy', () => expect(ellipse.xy.toString()).toEqual('1,2'));
+  // "bounding sphere"
+  it('bs', () => expect(ellipse.bs().toString()).toMatch(/Circle.x=1,y=2,radius=2/)); // TODO: radius twice what it should be?
+  it('center', () => expect(ellipse.center.toString()).toEqual('1,2'));
+
+  it('transform', () => expect(ellipse.transform([2,1],0).toString()).toEqual('Ellipse(x=2,y=2,radiusX=2NaN1)'));
+
+  it('radiusX', () => expect(ellipse.radiusX).toEqual(1));
+  it('radiusY', () => expect(ellipse.radiusY).toEqual(2));
+
+  // it('shift', () => expect(rect.shift()).toEqual('???'));
+  // it('shiftImpl', () => expect(rect.shiftImpl()).toEqual('???'));
+  // TODO: shiftImpl modifies object in place. Would immutability be a good thing?
+});
+
+describe('Polygon', () => {
+  var poly = new Polygon([new Vector2D(0,0), new Vector2D(2,0), new Vector2D(0,2)]);
+
+  describe('aabb', () => {
+    var aabb = poly.aabb();
+    it('x', () => expect(aabb.x).toEqual(0));
+    it('x2', () => expect(aabb.x2).toEqual(2));
+    it('y', () => expect(aabb.y).toEqual(0));
+    it('y2', () => expect(aabb.y2).toEqual(2));
+    it('h', () => expect(aabb.h).toEqual(2));
+    it('w', () => expect(aabb.w).toEqual(2));
+    // TODO: more
+  });
+
+  it('area', () => expect(poly.area).toEqual(2));
+
+  describe('asIntersectionParams', () => {
+    var params = poly.asIntersectionParams();
+    it('name', () => expect(params.name).toEqual('Polygon'));
+    // TODO
+  });
+
+  describe('corner', () => {
+    function corner(label, expected) {
+      it(label, () => expect(poly.corner(CORNER[label]).toString()).toEqual(expected));
+    }
+    corner('NW', '0,0');
+    corner('NE', '2,0');
+    corner('SE', '2,2');
+    corner('SW', '0,2');
+    corner('N', '1,0');
+    corner('S', '1,2');
+    corner('E', '2,1');
+    corner('W', '0,1');
+  });
+
+  describe('intersects', () => {
+    it('self', () => expect(poly.intersects(new Rect(0,0,2,2))));
+    it('outside', () => expect(! poly.intersects(new Rect(-1,-1,4,4))));
+    it('inside', () => expect(! poly.intersects(new Rect(0.5,0.5,1,1))));
+    it('touch', () => expect(poly.intersects(new Rect(-1,-1,1,1))));
+    it('overlap', () => expect(poly.intersects(new Rect(-1,-1,2,2))));
+  });
+
+  it('toString', () => expect(poly.toString()).toEqual('Polygon(0,0,2,0,0,2)'));
+  // "bounding sphere"
+  it('bs', () => expect(poly.bs().toString()).toMatch(/Circle.x=0.666*,y=0.666*,radius=1.49/)); // TODO: x,y should be 1,1?
+  it('center', () => expect(poly.center.toString()).toMatch(/0.666*,0.666*/));
+
+  it('transform', () => expect(poly.transform([2,1],0).toString()).toEqual('Polygon(0,0,4,0,0,2)'));
+
+  it('centroid', () => expect(poly.centroid.toString()).toMatch(/0.666*,0.666*/));
+  it('isClockwise', () => expect(poly.isClockwise).toEqual(false));
+  it('isConcave', () => expect(poly.isConcave).toEqual(false));
+  it('isConvex', () => expect(poly.isConvex).toEqual(true));
+  it('isCounterClockwise', () => expect(poly.isCounterClockwise).toEqual(true));
+  it('length', () => expect(poly.length).toEqual(3)); // NOT perimeter
+  it('pointInPolygon', () => expect(poly.pointInPolygon(new Vector2D(0.6,0.6))).toEqual(true));
+  it('pointInPolygon', () => expect(poly.pointInPolygon(new Vector2D(6,6))).toEqual(false));
+
+  // it('shift', () => expect(rect.shift()).toEqual('???'));
+  // it('shiftImpl', () => expect(rect.shiftImpl()).toEqual('???'));
+  // TODO: shiftImpl modifies object in place. Would immutability be a good thing?
+});
+
+describe('Line', () => {
+  var line = new Line(0,0,2,2);
+  describe('aabb', () => {
+    var aabb = line.aabb();
+    it('x', () => expect(aabb.x).toEqual(0));
+    it('x2', () => expect(aabb.x2).toEqual(2));
+    it('y', () => expect(aabb.y).toEqual(0));
+    it('y2', () => expect(aabb.y2).toEqual(2));
+    it('h', () => expect(aabb.h).toEqual(2));
+    it('w', () => expect(aabb.w).toEqual(2));
+    // TODO: more
+  });
+
+  describe('asIntersectionParams', () => {
+    var params = line.asIntersectionParams();
+    it('name', () => expect(params.name).toEqual('Line'));
+    it('x', () => expect(params.params[0].x).toEqual(0));
+    it('y', () => expect(params.params[0].y).toEqual(0));
+    it('x', () => expect(params.params[1].x).toEqual(2));
+    it('y', () => expect(params.params[1].y).toEqual(2));
+  });
+
+  describe('corner', () => {
+    function corner(label, expected) {
+      it(label, () => expect(line.corner(CORNER[label]).toString()).toEqual(expected));
+    }
+    corner('NW', '0,0');
+    corner('NE', '2,0');
+    corner('SE', '2,2');
+    corner('SW', '0,2');
+    corner('N', '1,0');
+    corner('S', '1,2');
+    corner('E', '2,1');
+    corner('W', '0,1');
+  });
+
+  describe('intersects', () => {
+    it('self', () => expect(line.intersects(new Rect(0,0,2,2))));
+    it('outside', () => expect(! line.intersects(new Rect(-1,-1,4,4))));
+    it('inside', () => expect(! line.intersects(new Rect(0.5,0.5,1,1))));
+    it('touch', () => expect(line.intersects(new Rect(-1,-1,1,1))));
+    it('overlap', () => expect(line.intersects(new Rect(-1,-1,2,2))));
+  });
+
+  it('toString', () => expect(line.toString()).toEqual('Line(x1=0,y1=0,x2=2NaN2)'));
+  it('xy', () => expect(line.xy.toString()).toEqual('0,0'));
+  // "bounding sphere"
+  it('bs', () => expect(line.bs().toString()).toEqual('Circle(x=1,y=1,radius=1)')); // TODO: radius twice what it should be?
+  it('center', () => expect(line.center.toString()).toEqual('1,1'));
+
+  it('transform', () => expect(line.transform([2,1],0).toString()).toEqual('Line(x1=0,y1=0,x2=4NaN2)'));
 
 });
