0b0c758bd4fb35fb7696e0f9697cc01b57aaaa3e
Holger Stitz <holger.stitz@jku.at>
2017-01-17 14:48:55 +0100

    Removed SimHash.shadeColor() and replace with d3.hsl().brighter()

--- src/provenance/SimilarityHash.ts
+++ src/provenance/SimilarityHash.ts
@@ -17,20 +17,6 @@ export class SimHash extends EventHandler {
   private static readonly NUMBER_OF_BITS:number = 300;
   private static readonly HASH_TABLE_SIZE:number = 1000;
 
-  public static shadeColor(color, percent) {
-    /*jshint bitwise:false */
-    /*tslint:disable:no-bitwise */
-    const f = parseInt(color.slice(1), 16);
-    const t = percent < 0 ? 0 : 255;
-    const p = percent < 0 ? percent * -1 : percent;
-    const R = f >> 16;
-    const G = f >> 8 & 0x00FF;
-    const B = f & 0x0000FF;
-    return '#' + (0x1000000 + (Math.round((t - R) * p) + R) * 0x10000 + (Math.round((t - G) * p) + G) * 0x100 + (Math.round((t - B) * p) + B)).toString(16).slice(1);
-    /*jshint bitwise:true */
-    /*tslint:enable:no-bitwise */
-  }
-
   public static getCategoryColor(category:string) {
     return SimHash.COLORS[SimHash.CATEGORIES.indexOf(category)];
   }
