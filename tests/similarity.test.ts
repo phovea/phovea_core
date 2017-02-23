55f40b43fcf0994f1d54a32b2080a843ee79582e
Holger Stitz <holger.stitz@jku.at>
2017-02-23 17:40:18 +0100

    Extraced murmurhash function to own file

--- tests/similarity.test.ts
+++ tests/similarity.test.ts
@@ -2,10 +2,9 @@
  * Created by Holger Stitz on 23.02.2017.
  */
 
-import {HashBuilder, murmurhash2_32_gc} from '../src/provenance/internal/HashBuilder';
-import {murmurhash2_32_gc} from '../src/provenance/internal/HashBuilder';
+import {murmurhash2} from '../src/provenance/internal/MurmurHash2';
 
-describe('murmurhash2_32_gc', () => {
+describe('murmurhash2', () => {
   const str42 = '42';
   const num42 = 42;
   const hashFor42 = '10011000101001111000010010110';
@@ -14,28 +13,29 @@ describe('murmurhash2_32_gc', () => {
 
   it('check if result is a string', () => {
     // @see string check from http://stackoverflow.com/a/4891964/940219
-    return expect(Object.prototype.toString.call(murmurhash2_32_gc(str42, seed)))
+    return expect(Object.prototype.toString.call(murmurhash2(str42, seed)))
       .toEqual('[object String]');
   });
 
   it('compare generated hash to static value', () => {
-    return expect(murmurhash2_32_gc(str42, seed))
+    return expect(murmurhash2(str42, seed))
       .toEqual(hashFor42);
   });
 
   it('check reproducibility of hash', () => {
-    return expect(murmurhash2_32_gc(str42, seed))
-      .toEqual(murmurhash2_32_gc(str42, seed));
+    return expect(murmurhash2(str42, seed))
+      .toEqual(murmurhash2(str42, seed));
   });
 
   it('check cast from number to string', () => {
-    return expect(murmurhash2_32_gc(num42, seed))
-      .toEqual(murmurhash2_32_gc(str42, seed));
+    return expect(murmurhash2(num42, seed))
+      .toEqual(murmurhash2(str42, seed));
   });
 
   it('check if different seed return different hash', () => {
-    return expect(murmurhash2_32_gc(str42, seed))
-      .not.toEqual(murmurhash2_32_gc(str42, seed2));
+    return expect(murmurhash2(str42, seed))
+      .not.toEqual(murmurhash2(str42, seed2));
   });
 });
-});
+
+
