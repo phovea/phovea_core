4d3e0a0cd43b8c141fb538421d4e06eae23e8d63
anita-steiner <k9908600@gmx.at>
2018-10-30 11:24:06 +0100

    add webpack constants to jest

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,13 @@ module.exports = {
         tsConfigFile: 'tsconfig.test.json',
         enableTsDiagnostics: true,
         skipBabel: true
-      }
+      },
+      __VERSION__: '1.0.0-test',
+      __LICENSE__: 'Test-licence',
+      __BUILD_ID__: 0,
+      __DEBUG__: true,
+      __TEST__: true,
+      __PRODUCTION__: false,
+      __APP_CONTEXT__: '/'
     }
   };
