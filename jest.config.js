8b37ba0805b3e202087bbe63ed77234f4f51eedd
Holger Stitz <holger.stitz@datavisyn.io>
2019-09-06 15:37:46 +0200

    Update jest.config.js syntax to match v24
    Deactivated the verbose flag

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,28 @@
 module.exports = {
-    transform: {
-      '^.+\\.(t|j)sx?$': 'ts-jest'
-    },
-    testRegex: "tests/.*\\.(ts|tsx|js)$",
-    moduleFileExtensions: [
-      'ts',
-      'tsx',
-      'js'
-    ],
-    verbose: true,
-    globals: {
-      'ts-jest': {
-        tsConfigFile: 'tsconfig.test.json',
-        enableTsDiagnostics: true,
-        skipBabel: true
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  testRegex: "tests/.*\\.(ts|tsx|js)$",
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js'
+  ],
+  verbose: false,
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+      diagnostics: {
+        warnOnly: true,
       },
-      __VERSION__: '1.0.0-test',
-      __LICENSE__: 'Test-licence',
-      __BUILD_ID__: 0,
-      __DEBUG__: true,
-      __TEST__: true,
-      __PRODUCTION__: false,
-      __APP_CONTEXT__: '/'
-    }
-  };
+    },
+    __VERSION__: '1.0.0-test',
+    __LICENSE__: 'Test-licence',
+    __BUILD_ID__: 0,
+    __DEBUG__: true,
+    __TEST__: true,
+    __PRODUCTION__: false,
+    __APP_CONTEXT__: '/',
+  },
+  preset: 'ts-jest',
+};
