6adaebc4c56805ade1015def10312eca0b9600c5
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-01-28 15:25:46 +0100

    update _jest.config.js_
    according to setup in triage

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,50 @@
+// test dependencies that require transformation
+const pluginsToTransform = [
+  'tdp_comments',
+  'tdp_ui',
+  'tdp_core',
+  'bootstrap-sass', // required to transform for phovea_ui
+  'phovea_ui',
+  'phovea_clue',
+  'phovea_core',
+  'phovea_security_flask',
+  'sandbox',
+  'tdp_marvinjs'
+].join('|');
+
+/**
+ * TODO check if we can process inline webpack loaders (e.g. as found in https://github.com/phovea/phovea_ui/blob/master/src/_bootstrap.ts)
+ * see also https://jestjs.io/docs/en/webpack#mocking-css-modules
+ */
 module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
-  testRegex: "tests/.*\\.(ts|tsx|js)$",
+  testRegex: "(.*(test|spec))\\.(tsx?)$",
+  testURL: "http://localhost/",
   moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js'
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
   ],
-  verbose: false,
+  modulePaths: [
+    "src",
+    "../node_modules",
+    "../"
+  ],
+  transformIgnorePatterns: [`../node_modules/(?!${pluginsToTransform})`],
   globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.test.json',
-      diagnostics: {
-        warnOnly: true,
-      },
-    },
-    __VERSION__: '1.0.0-test',
-    __LICENSE__: 'Test-licence',
-    __BUILD_ID__: 0,
-    __DEBUG__: true,
-    __TEST__: true,
-    __PRODUCTION__: false,
-    __APP_CONTEXT__: '/',
+    "__VERSION__": "TEST_VERSION",
+    "__APP_CONTEXT__": "TEST_CONTEXT"
   },
-  testURL: 'http://localhost/'
-};
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "imports-loader?.*": "imports-loader",
+    "raw-loader?.*": "raw-loader",
+    "file-loader?.*": "file-loader",
+    "script-loader?.*": "script-loader"
+  }
+}
