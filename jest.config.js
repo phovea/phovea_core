74904d590ccce3c388202ba28499cdc1596c3944
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-01-28 13:03:39 +0100

    replace _karma.conf.js_ with _jest.config.js_

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "tests/.*\\.(ts|tsx|js)$",
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js'
+  ],
+  verbose: false,
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+      diagnostics: {
+        warnOnly: true,
+      },
+    },
+    __VERSION__: '1.0.0-test',
+    __LICENSE__: 'Test-licence',
+    __BUILD_ID__: 0,
+    __DEBUG__: true,
+    __TEST__: true,
+    __PRODUCTION__: false,
+    __APP_CONTEXT__: '/',
+  },
+  testURL: 'http://localhost/'
+};
