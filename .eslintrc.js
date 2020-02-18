41727a844ffab0019e17da116d2c4b52bc11965a
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-02-18 09:15:31 +0100

    add _.babelrc_ and _.eslintrc.js_

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowTypedFunctionExpressions: true,
+      },
+    ],
+    '@typescript-eslint/indent': ['error', 2],
+    'react/prop-types': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
