aa98b2a0678e0d6712a3afe704a19b32a7539ad5
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-03-30 15:51:19 +0200

    update development configuration

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,13 +1,20 @@
 const merge = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common');
+const pkg = require('./../package.json');
+
+
+let libName = /phovea_.*/.test(pkg.name) ? ['phovea', pkg.name.slice(7)] : pkg.name;
 
 const config = {
   mode: 'development',
   devtool: 'inline-source-map',
   output: {
-    path: path.join(__dirname, '../build'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, './../build'),
+    filename: '[name].[contenthash:8].js',
+    publicPath: '/',
+    library: libName,
+    libraryTarget: 'umd'
   },
 };
 
