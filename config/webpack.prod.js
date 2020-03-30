e1017c8fca67faf93ccb581925395024a2d7e1e2
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-03-30 15:50:10 +0200

    update imports and add variable for libname

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,13 @@
 const merge = require('webpack-merge');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const common = require('./webpack.common');
+const pkg = require('./../package.json');
+
+
+let libName = /phovea_.*/.test(pkg.name) ? ['phovea', pkg.name.slice(7)] : pkg.name;
 
 const config = {
   mode: 'production',
