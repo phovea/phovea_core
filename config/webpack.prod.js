3293b8fc76f57408c9e691ad8e4dfb192266bbec
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-03-30 15:50:32 +0200

    update plugins for production

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -17,7 +17,29 @@ const config = {
     filename: 'js/main.[contentHash].js',
     publicPath: './',
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    // TerserPlugin is added by default in production mode
+    // extracts css in separate file
+    new MiniCssExtractPlugin({
+      filename: 'styles.[name].[contenthash].css'
+    }),
+    // cleans output.path folder (removes old files before new build)
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        '**/*',
+        path.join(process.cwd(), 'dist/**/*')
+      ]
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      // which bundles should be included; vendors includes common libraries
+      chunks: ['index', 'vendors_index'],
+      title: 'Hello World Chunk',
+      meta: {
+        description: 'chunk description'
+      }
+    })
+  ]
 };
 
 module.exports = merge(common, config);
